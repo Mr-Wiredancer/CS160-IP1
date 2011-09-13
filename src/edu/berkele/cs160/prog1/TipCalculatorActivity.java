b18d73531b932c187ee6490be3d19e7e8b380591
Jiahao Li <lijiahao90@gmail.com>
2011-09-13 11:07:54 -0700

    first commit

---
+++ src/edu/berkele/cs160/prog1/TipCalculatorActivity.java
@@ -0,0 +1,13 @@
+package edu.berkele.cs160.prog1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
